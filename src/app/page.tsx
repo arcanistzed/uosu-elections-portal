d9dda999736cb06cdfa643d25c7c419b1a059f51
Daniel Thorp <arcanistzed@gmail.com>
2024-04-30 19:13:29 +0000

    feat: performance improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,16 @@
 "use client";
 
-import { SessionProvider, signIn, useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { signIn } from "next-auth/react";
 import { useEffect } from "react";
-import { api } from "~/trpc/react";
 
 export default function Home() {
-	return (
-		<SessionProvider>
-			<Main />
-		</SessionProvider>
-	);
-}
-
-function Main() {
-	const session = useSession();
-
-	const accessKey = api.accessKey.getByEmail.useQuery({
-		email: session.data?.user.email ?? "",
+	useEffect(() => {
+		void signIn("azure-ad", { callbackUrl: `/vote` });
 	});
 
-	useEffect(() => {
-		if (!session.data) {
-			void signIn("azure-ad");
-		}
-	}, [session.data]);
-
-	useEffect(() => {
-		if (accessKey.data) {
-			redirect(`https://secure.electionbuddy.com/${accessKey.data.key}`);
-		}
-	}, [accessKey.data]);
-
 	return (
-		<div>
-			{accessKey.error ? (
-				<div>{accessKey.error.message}</div>
-			) : (
-				<div>Chargement... Loading...</div>
-			)}
-		</div>
+		<main className="flex items-center justify-center p-2 text-white">
+			<h1 className="text-3xl font-bold">Chargement... Loading...</h1>
+		</main>
 	);
 }
